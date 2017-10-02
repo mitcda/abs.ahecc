5108da073dbd72f6716337b2a626287daba7e726
= <=>
2017-10-02 21:49:37 +1100

    Added AHECC 2007--2017 and classification codes and abbreviations

--- R/data.R
+++ R/data.R
@@ -1,33 +1,78 @@
-#' @title Standard Australian Classification of Countries 2016
-#' @description  A data set containing the Standard Australian Classification of Countries (SACC), 2016.
-#' @format Data set \code{sacc2016} contains the 2016 SACC, which contains 255 individual country codes. The data sets \code{saccCorr_2016_2011} and \code{saccCorr_2011_2016} provide concordances between the 2016 SACC and 2011 SACC.  The datasets contain the following variables:
+#' @title Australian Harmonized Export Commodity Classification (AHECC)
+#' @description  A data set containing the Australian Harmonized Export Commodity Classification (AHECC).
+#' @format Data sets \code{ahecc_2017}, \code{ahecc_2012} and \code{ahecc_2007} contain the respective AHECCs.
+#'   The datasets contain the following variables:
 #' \describe{
-#'   \item{major_group_code}{The major group level is the highest level of the classification. The SACC contains nine major groups, each containing between two and six geographically proximate minor groups.}
-#'   \item{major_group_name}{Major group level name.}
-#'   \item{minor_group_code}{The minor group level is the middle level of the classification.  The SACC contains 27 minor groups, containing approximately five to twenty country categories.}
-#'   \item{minor_group_name}{Minor group level name.}
-#'   \item{country_code}{The country level is the base level of the classification. There are 255 country categories including four residual ('not elsewhere classified') categories. Residual categories are explained in 'About Codes'.}
-#'   \item{country_name}{Country names}
-#'   \item{country_name_15char}{Short country names for publication -- this list comprises country and group titles shortened to a maximum field length of 15 characters.}
-#'   \item{country_name_30char}{Short country names for publication -- this list comprises country and group titles shortened to a maximum field length of 30 characters.}
-#'   \item{un_region_code}{United Nations' (UN) Standard Region Code.}
-#'   \item{un_country_code}{United Nations' (UN) Standard Country Code.}
+#'   \item{ahecc_code}{12-digit AHECC commodity code}
+#'   \item{chapter_code}{Two-digit AHECC chapter code}
+#'   \item{chapter_label}{AHECC chapter label}
+#'   \item{heading_code}{Four-digit AHECC heading code}
+#'   \item{heading_label}{Five-digit AHECC heading label}
+#'   \item{hs5_code}{Five-digit AHECC heading code}
+#'   \item{hs5_label}{Five-digit AHECC heading label}
+#'   \item{hs_code}{Six-digit AHECC heading code}
+#'   \item{hs_label}{Six-digit AHECC heading label}
+#'   \item{hs7_code}{Seven-digit AHECC heading code}
+#'   \item{hs7_label}{Seven-digit AHECC heading label}
+#'   \item{export_item}{Eight-digit AHECC export item code}
+#'   \item{quantity_unit}{Unit of quantity}
+#'   \item{description}{AHECC export item description}
+#'   \item{free_descriptor}{Free-standing description.}
 #' }
-#' @details The Standard Australian Classification of Countries (SACC) provides guidelines for
-#'   consistent collection, aggregation and dissemination of statistics by country. The classification
-#'   is used by the Australian Bureau of Statistics (ABS) when collecting, aggregating and disseminating
-#'   data relating to characteristics such as birthplace and country of residence. In addition to its
-#'   use by the ABS, the SACC is also designed for use in the broader Australian statistical community,
-#'   including government agencies, private companies and community organisations.
-#' 
-#'   The country names within the SACC reflect country titles recognised by the Australian Government.
-#'   The SACC reflects the official recognition of country titles by the Australian Government, which
-#'   the ABS updates as necessary to ensure the classification remains current.
+#' @details The Australian Harmonized Export Commodity Classification (AHECC) is
+#'   designed for use by exporters, customs brokers and freight forwarders in the
+#'   classification of goods when providing export declarations to the Department
+#'   of Immigration and Border Protection, and to assist users interpret export
+#'   statistics published by the Australian Bureau of Statistics (ABS).
+#'
+#'   The classification is based on the six digit international Harmonized Commodity
+#'   Description and Coding System (HS) developed by the World Customs Organization
+#'   (WCO) for describing internationally traded goods. The ABS extends the six digit
+#'   international HS by two digits to provide a finer level of detail to meet
+#'   Australian statistical requirements (Statistical codes).
 #' 
 #'   For further details, see:
-#'   \href{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1269.0}{Standard Australian Classification of Countries (SACC)}.
-#' @source The SACC is produced and maintained by the Australian Bureau of Statistics. 
-#' @references Australian Bureau of Statistics 2016, \ital{Standard Australian Classification of Countries (SACC), 2016}, Catalogue no. 1269.0, ABS, Canberra. URL: \url{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1269.0}.
-"sacc2016"
-"saccCorr_2016_2011"
-"saccCorr_2011_2016"
+#'   \href{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1233.0}{Australian Harmonized Export Commodity Classification (AHECC)}.
+#' @source The AHECC is produced and maintained by the Australian Bureau of Statistics. 
+#' @references Australian Bureau of Statistics 2016, \emph{Australian Harmonized Export Commodity Classification (AHECC), 2017}, Catalogue no. 1233.0, ABS, Canberra. URL: \url{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1233.0}.
+"ahecc_2017"
+"ahecc_2012"
+"ahecc_2007"
+
+
+#' @title AHECC Classification Codes and Abbreviations
+#' @description  Data set containing the Australian Harmonized Export Commodity
+#'   Classification (AHECC) supporting codes and abbreviations
+#' @format Data sets \code{ahecc_uq}, \code{ahecc_abb}, \code{ahecc_currencies}
+#'   and \code{ahecc_countries} contain supporting .
+#'   The datasets contain the following variables:
+#' \describe{
+#'   \item{abbreviation}{unit of quantity abbreviation or AHECC abbreviation codes}
+#'   \item{description}{unit of quantity description or AHECC abbreviation description}
+#'   \item{country}{Country name}
+#'   \item{currency_code}{Three-character country currency code}
+#'   \item{currency}{Currency description}
+#'   \item{code}{Two-character country code}
+#'   \item{notes}{AHECC country notes.}
+#' }
+#' @details The Australian Harmonized Export Commodity Classification (AHECC) is
+#'   designed for use by exporters, customs brokers and freight forwarders in the
+#'   classification of goods when providing export declarations to the Department
+#'   of Immigration and Border Protection, and to assist users interpret export
+#'   statistics published by the Australian Bureau of Statistics (ABS).
+#'
+#'   These data sets provide supporting AHECC Classification Codes and Abbreviations:
+#'   \itemize{
+#'     \item{Unit of Quantity (\code{ahecc_uq})}
+#'     \item{Abbreviations (\code{ahecc_abb})}
+#'     \item{FOB Currency Codes (\code{ahecc_currencies})}
+#'     \item{Countries (\code{ahecc_countries})}
+#'   }
+#'   For further details, see:
+#'   \href{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1233.0}{Australian Harmonized Export Commodity Classification (AHECC)}.
+#' @source The AHECC is produced and maintained by the Australian Bureau of Statistics. 
+#' @references Australian Bureau of Statistics 2016, \emph{Australian Harmonized Export Commodity Classification (AHECC), 2017}, Catalogue no. 1233.0, ABS, Canberra. URL: \url{http://www.abs.gov.au/ausstats/abs@.nsf/mf/1233.0}.
+"ahecc_uq"
+"ahecc_abb"
+"ahecc_currencies"
+"ahecc_countries"
